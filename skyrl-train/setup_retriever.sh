79b25b4af500a42d035f45e929039e9e6ddfef4e
quanwei0 <weiquan0128@gmail.com>
2026-01-13 22:42:12 -0600

    update

--- skyrl-train/setup_retriever.sh
+++ skyrl-train/setup_retriever.sh
@@ -14,11 +14,3 @@ conda install faiss-gpu==1.8.0 -c pytorch -c nvidia -y
 
 # Install the API service framework
 pip install uvicorn fastapi
-
-
-conda activate retriever
-
-local_dir=~/data/searchR1
-python examples/search/searchr1_download.py --local_dir $local_dir
-cat $local_dir/part_* > $local_dir/e5_Flat.index
-gzip -d $local_dir/wiki-18.jsonl.gz
--- skyrl-train/setup_search.sh
+++ skyrl-train/setup_search.sh
@@ -2,7 +2,3 @@ curl -LsSf https://astral.sh/uv/install.sh | sh
 
 # creates a venv at .venv/
 uv sync --extra vllm 
-source .venv/bin/activate
-
-local_dir=~/data/searchR1
-uv run --isolated examples/search/searchr1_dataset.py --local_dir $local_dir
